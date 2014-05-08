28afcf3117e1d565fd44b51af92b468cfd79c6cb
Satoshi KOJIMA <skoji@mac.com>
2014-05-08 18:21:31 +0900

    use frontmatter to specify generator

--- nora_mark_htmlbook.gemspec
+++ nora_mark_htmlbook.gemspec
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_runtime_dependency "nora_mark", ">= 0.2beta8"
+  spec.add_runtime_dependency "nora_mark", ">= 0.2beta10"
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rspec", "~> 2.14"
   spec.add_development_dependency "rake"
--- spec/htmlbook_spec.rb
+++ spec/htmlbook_spec.rb
@@ -1,98 +1,106 @@
 # -*- encoding: utf-8 -*-
 require 'nora_mark'
 require File.dirname(__FILE__) + '/spec_helper.rb'
-require File.dirname(__FILE__) + '/../lib/nora_mark_htmlbook.rb'
 require 'nokogiri'
 require File.dirname(__FILE__) + '/nokogiri_test_helper.rb'
 
-describe 'html_book plugin' do
-  before do
-    NoraMark::Extensions.register_generator(NoraMark::Htmlbook::Generator)
-  end
-  
-  it 'generate body with data-book' do
-    text = "---\ntitle: the title.\n---\ndocument."
-    parsed = NoraMark::Document.parse(text, lang: 'ja')
-    xhtml = parsed.htmlbook
-    body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')
-    expect(body.selector_and_children)
-      .to eq(
-             ["body[data-type='book']",
-              ['h1', 'the title.'],
-              ['div.pgroup',
-               ['p', 'document.']]]
-             )
-  end
-  it 'generate chapter  ' do
-    text = "---\ntitle: the title.\n---\n# chapter title\ndocument."
-    parsed = NoraMark::Document.parse(text, lang: 'ja')
-    xhtml = parsed.htmlbook
-    body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')    
-    expect(body.selector_and_children)
-      .to eq(
-             ["body[data-type='book']",
-              ['h1', 'the title.'],
-              ["section[data-type='chapter']",
-               ['h1', 'chapter title'],
-               ['div.pgroup',
-                ['p', 'document.']]]]
-             )
-  end
-  it 'generate chapter and section' do
-    text = "---\ntitle: the title.\n---\n# chapter title\n## section title\ndocument."
-    parsed = NoraMark::Document.parse(text, lang: 'ja')
-    xhtml = parsed.htmlbook
-    body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')    
-    expect(body.selector_and_children)
-      .to eq(
-             ["body[data-type='book']",
-              ['h1', 'the title.'],
-              ["section[data-type='chapter']",
-               ['h1', 'chapter title'],
-               ["section[data-type='sect1']",
-                ['h1', 'section title'],
-                ['div.pgroup',
-                 ['p', 'document.']]]]]
-             )
-  end
-  it 'generate appendix and section' do
-    text = "---\ntitle: the title.\n---\n#(appendix) appendix title\n## section title\ndocument."
-    parsed = NoraMark::Document.parse(text, lang: 'ja')
-    xhtml = parsed.htmlbook
-    body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')    
-    expect(body.selector_and_children)
-      .to eq(
-             ["body[data-type='book']",
-              ['h1', 'the title.'],
-              ["section[data-type='appendix']",
-               ['h1', 'appendix title'],
-               ["section[data-type='sect1']",
-                ['h1', 'section title'],
-                ['div.pgroup',
-                 ['p', 'document.']]]]]
-             )
-  end
+$:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 
-  it 'generate sidebar' do
-    text = <<EOF
+describe 'html_book plugin' do
+  describe 'explicitly load extension' do
+    before(:all) do
+      NoraMark::Extensions.register_generator(:htmlbook)
+    end
+    
+    it 'generate body with data-book' do
+      text = "---\ntitle: the title.\n---\ndocument."
+      parsed = NoraMark::Document.parse(text, lang: 'ja')
+      xhtml = parsed.htmlbook
+      body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')
+      expect(body.selector_and_children)
+        .to eq(
+               ["body[data-type='book']",
+                ['h1', 'the title.'],
+                ['div.pgroup',
+                 ['p', 'document.']]]
+               )
+    end
+  end
+  describe 'declare generator in the frontmatter' do
+    before(:each) do
+      NoraMark::Extensions.unregister_generator(:htmlbook)
+    end
+    it 'generate chapter  ' do
+      text = "---\ntitle: the title.\ngenerator: htmlbook\n---\n# chapter title\ndocument."
+      parsed = NoraMark::Document.parse(text, lang: 'ja')
+      xhtml = parsed.htmlbook
+      body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')    
+      expect(body.selector_and_children)
+        .to eq(
+               ["body[data-type='book']",
+                ['h1', 'the title.'],
+                ["section[data-type='chapter']",
+                 ['h1', 'chapter title'],
+                 ['div.pgroup',
+                  ['p', 'document.']]]]
+               )
+    end
+    it 'generate chapter and section' do
+      text = "---\ntitle: the title.\ngenerator: htmlbook\n---\n# chapter title\n## section title\ndocument."
+      parsed = NoraMark::Document.parse(text, lang: 'ja')
+      xhtml = parsed.htmlbook
+      body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')    
+      expect(body.selector_and_children)
+        .to eq(
+               ["body[data-type='book']",
+                ['h1', 'the title.'],
+                ["section[data-type='chapter']",
+                 ['h1', 'chapter title'],
+                 ["section[data-type='sect1']",
+                  ['h1', 'section title'],
+                  ['div.pgroup',
+                   ['p', 'document.']]]]]
+               )
+    end
+    it 'generate appendix and section' do
+      text = "---\ntitle: the title.\ngenerator: htmlbook\n---\n#(appendix) appendix title\n## section title\ndocument."
+      parsed = NoraMark::Document.parse(text, lang: 'ja')
+      xhtml = parsed.htmlbook
+      body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')    
+      expect(body.selector_and_children)
+        .to eq(
+               ["body[data-type='book']",
+                ['h1', 'the title.'],
+                ["section[data-type='appendix']",
+                 ['h1', 'appendix title'],
+                 ["section[data-type='sect1']",
+                  ['h1', 'section title'],
+                  ['div.pgroup',
+                   ['p', 'document.']]]]]
+               )
+    end
+
+    it 'generate sidebar' do
+      text = <<EOF
 ---
 title: the title.
+generator: htmlbook
 ---
 sidebar(Amusing Digression) {
 Did you know that in Boston, they call it "soda", and in Chicago, they call it "pop"?
 }
 EOF
-    parsed = NoraMark::Document.parse(text)
-    xhtml = parsed.htmlbook
-    body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')
-    expect(body.selector_and_children)
-      .to eq(
-             ["body[data-type='book']",
-              ['h1', 'the title.'],
-              ["aside[data-type='sidebar']",
-               ['h5', 'Amusing Digression'],
-               ['p', 'Did you know that in Boston, they call it "soda", and in Chicago, they call it "pop"?']]]
-             )
+      parsed = NoraMark::Document.parse(text)
+      xhtml = parsed.htmlbook
+      body = Nokogiri::XML::Document.parse(xhtml).root.at_xpath('xmlns:body')
+      expect(body.selector_and_children)
+        .to eq(
+               ["body[data-type='book']",
+                ['h1', 'the title.'],
+                ["aside[data-type='sidebar']",
+                 ['h5', 'Amusing Digression'],
+                 ['p', 'Did you know that in Boston, they call it "soda", and in Chicago, they call it "pop"?']]]
+               )
+    end
   end
-
 end
